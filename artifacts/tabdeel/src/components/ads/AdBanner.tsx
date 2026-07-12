708274d233fbdb4652324e26dff6ce299ca3dbd7
aamohammadalhulisi-del <aamohammadalhulisi@gmail.com>
2026-07-12 07:58:03 +0300

    fix adsense width

--- artifacts/tabdeel/src/components/ads/AdBanner.tsx
+++ artifacts/tabdeel/src/components/ads/AdBanner.tsx
@@ -16,10 +16,14 @@ export default function AdBanner() {
   }, []);
 
   return (
-    <div className="w-full flex justify-center my-6">
+    <div className="w-full max-w-[600px] min-h-[280px] mx-auto my-6">
       <ins
         className="adsbygoogle"
-        style={{ display: "block" }}
+        style={{
+          display: "block",
+          width: "100%",
+          minHeight: "280px",
+        }}
         data-ad-format="fluid"
         data-ad-layout-key="-fb+5w+4e-db+86"
         data-ad-client="ca-pub-3491956593400214"
